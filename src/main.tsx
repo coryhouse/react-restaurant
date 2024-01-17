259d4ef84c625f73b454cd184dc4c8e5fe3f7ebb
Cory House <housecor@gmail.com>
2024-01-17 10:38:02 -0600

    add

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const foods = ["pizza", "hamburger", "taco"];
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hi</h1>
+    <h1>Menu</h1>
+    <ul>
+      {foods.map((food) => (
+        <li key={food}>{food}</li>
+      ))}
+    </ul>
   </React.StrictMode>
 );
