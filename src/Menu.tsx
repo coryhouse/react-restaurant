4309694064d90b19c999a8e5c7afd9fb6eb891c3
Cory House <housecor@gmail.com>
2024-01-17 11:14:11 -0600

    add

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,12 +1,27 @@
-const foods = ["pizza", "hamburger", "taco"];
+import { Fragment, useState } from "react";
 
 export function Menu() {
+  const [foods, setFoods] = useState(["pizza", "hamburger", "taco"]);
+
   return (
     <>
       <h1>Menu</h1>
       <ul>
         {foods.map((food) => (
-          <li key={food}>{food}</li>
+          <Fragment key={food}>
+            <button
+              onClick={() => {
+                setFoods((prevFoods) => {
+                  const newFoods = prevFoods.filter((f) => f !== food);
+                  console.log(newFoods);
+                  return newFoods; // Whatever we return becomes the new state. Setting state is asynchronous (hey react, set the state ASAP)
+                });
+              }}
+            >
+              Delete
+            </button>{" "}
+            <li key={food}>{food}</li>
+          </Fragment>
         ))}
       </ul>
     </>
