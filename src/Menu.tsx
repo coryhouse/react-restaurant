3a392c145085e4e036810d66ff0ba1caeb82221d
Cory House <housecor@gmail.com>
2024-01-17 14:32:57 -0600

    add

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,5 +1,6 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Food } from "./food";
+import { Card } from "./Card";
 
 export function Menu() {
   const [foods, setFoods] = useState<Food[]>([]);
@@ -16,26 +17,39 @@ export function Menu() {
 
   return (
     <>
-      <h1 className="text-2xl font-bold">Menu</h1>
-      <img src="/images/burger.jpg" />
-      <ul>
+      <h1>Menu</h1>
+      <div className="flex flex-wrap">
         {foods.map((food) => (
-          <Fragment key={food.id}>
-            <button
-              onClick={() => {
-                setFoods((prevFoods) => {
-                  const newFoods = prevFoods.filter((f) => f !== food);
-                  console.log(newFoods);
-                  return newFoods; // Whatever we return becomes the new state. Setting state is asynchronous (hey react, set the state ASAP)
-                });
-              }}
-            >
-              Delete
-            </button>{" "}
-            <li>{food.name}</li>
-          </Fragment>
+          <Card key={food.id}>
+            <div className="flex justify-between">
+              <div className="w-48">
+                <h2>{food.name}</h2>
+                <button
+                  className="text-red-500 hover:cursor-pointer"
+                  onClick={() => {
+                    setFoods((prevFoods) => {
+                      const newFoods = prevFoods.filter((f) => f !== food);
+                      console.log(newFoods);
+                      return newFoods; // Whatever we return becomes the new state. Setting state is asynchronous (hey react, set the state ASAP)
+                    });
+                  }}
+                >
+                  Delete
+                </button>
+                <p>{food.description}</p>
+                <p>${food.price}</p>
+              </div>
+              <div className="w-36">
+                <img
+                  className="w-32 rounded"
+                  alt={food.name}
+                  src={`/images/${food.image}`}
+                />
+              </div>
+            </div>
+          </Card>
         ))}
-      </ul>
+      </div>
     </>
   );
 }
