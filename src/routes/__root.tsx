1b27dc0465c6d786e1ebcc1188db33b155eda176
Cory House <housecor@gmail.com>
2025-08-21 16:38:44 -0500

    fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-export const rootRoute = createRootRoute({
+export const Route = createRootRoute({
   component: () => (
     <>
       <nav className="flex gap-2 p-2">
