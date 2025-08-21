39406cc147ebb7a30d43691d7e05eb1855e5fc0f
Cory House <housecor@gmail.com>
2025-08-21 15:05:22 -0500

    a

--- src/food.ts
+++ src/food.ts
@@ -1,11 +1,15 @@
-export type Food = {
-  id: number;
-  name: string;
-  image: string;
-  price: number;
-  description: string;
-  tags: Array<FoodTag>;
-};
+import * as z from "zod";
+
+export const foodSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  image: z.string(),
+  price: z.number(),
+  description: z.string(),
+  tags: z.array(z.string()),
+});
+
+export type Food = z.infer<typeof foodSchema>;
 
 export type NewFood = Omit<Food, "id">;
 
