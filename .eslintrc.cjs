ffcae864ef1f7e2dd09941cbed8c9ba84c8bfd8d
Cory House <housecor@gmail.com>
2025-08-21 16:20:19 -0500

    ignore routeTree

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "src/routeTree.gen.ts"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
