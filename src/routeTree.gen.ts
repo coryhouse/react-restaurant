39747b426a6de087d59d4d0af195e9d8dbd69405
Cory House <housecor@gmail.com>
2025-08-21 16:27:33 -0500

    a

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,10 +8,13 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
+import { createRootRoute } from '@tanstack/react-router'
+
 import { Route as AdminRouteImport } from './routes/admin'
 import { Route as AboutRouteImport } from './routes/about'
 import { Route as IndexRouteImport } from './routes/index'
+
+const rootRouteImport = createRootRoute()
 
 const AdminRoute = AdminRouteImport.update({
   id: '/admin',
