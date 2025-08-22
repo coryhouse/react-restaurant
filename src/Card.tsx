149dd6122c09018e5fd69dc57e3e02ef35d3270a
Cory House <housecor@gmail.com>
2025-08-22 10:40:21 -0500

    color

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,7 +4,7 @@ type CardProps = {
 
 export function Card(props: CardProps) {
   return (
-    <section className="bg-sky-200 rounded shadow border-1 border-gray-500 m-2 p-2 mw-96">
+    <section className="bg-sky-100 rounded shadow border-1 border-gray-500 m-2 p-2 mw-96">
       {props.children}
     </section>
   );
