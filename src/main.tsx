2fc6ff03ec44114d2fdaf8dd7db26f7c1b2e7fcf
Cory House <housecor@gmail.com>
2025-08-23 11:20:40 -0500

    config queryclient

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, Router } from "@tanstack/react-router";
 import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { Toaster } from "sonner";
 import { UserContextProvider } from "./UserContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-// Create a new router instance
-const router = new Router({ routeTree });
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       throwOnError: true,
@@ -34,14 +24,34 @@ const queryClient = new QueryClient({
   },
 });
 
+// Create router instance
+const router = new Router({
+  routeTree,
+  context: {
+    queryClient,
+    // Since we're using React Query, we don't want loader calls to ever be stale
+    // This will ensure that the loader is always called when the route is preloaded or visited
+    defaultPreloadStaleTime: 0,
+  },
+  scrollRestoration: true,
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toaster richColors position="top-right" />
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <UserContextProvider>
-        <RouterProvider router={router} />
-      </UserContextProvider>
-    </QueryClientProvider>
+    <ErrorBoundary fallback={<div>Oops!</div>}>
+      <Toaster richColors position="top-right" />
+      <QueryClientProvider client={queryClient}>
+        <UserContextProvider>
+          <RouterProvider router={router} />
+        </UserContextProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
