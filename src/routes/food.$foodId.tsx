ee0c774a301f9e8855994b1f80d696e93f3abfc1
Cory House <housecor@gmail.com>
2025-08-29 08:28:42 -0500

    a

---
+++ src/routes/food.$foodId.tsx
@@ -0,0 +1,36 @@
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { FoodCard } from "../shared/FoodCard";
+import { FoodRatings } from "../shared/FoodRatings";
+import { z } from "zod";
+import { foodQueries } from "../query-factories/foods";
+import { useQuery } from "@tanstack/react-query";
+
+export const Route = createFileRoute("/food/$foodId")({
+  params: {
+    parse: (params) => ({
+      foodId: z.string().parse(params.foodId),
+    }),
+  },
+  component: FoodDetail,
+  loader: ({ context: { queryClient }, params: { foodId } }) => {
+    if (foodId) queryClient.ensureQueryData(foodQueries.getFoodById(foodId));
+  },
+});
+
+function FoodDetail() {
+  const { foodId } = Route.useParams();
+
+  const { data: existingFood } = useQuery({
+    ...foodQueries.getFoodById(foodId),
+    enabled: !!foodId,
+  });
+
+  if (!existingFood) throw notFound();
+
+  return (
+    <div>
+      <FoodCard food={existingFood} />
+      <FoodRatings foodId={existingFood.id} />
+    </div>
+  );
+}
