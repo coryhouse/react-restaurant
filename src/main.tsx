74d96057f0cd0ca8642f7ee6e5f81b90df761199
Cory House <housecor@gmail.com>
2025-12-30 20:11:08 -0600

    a

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "sonner";
 import { UserContextProvider } from "./UserContext";
+import { CartContextProvider } from "./CartContext";
 import { AppRouterProvider } from "./Router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./queryClient";
@@ -13,9 +14,11 @@ createRoot(document.getElementById("root")!).render(
     <ErrorBoundary fallback={<div>Oops!</div>}>
       <Toaster richColors position="top-right" />
       <UserContextProvider>
-        <QueryClientProvider client={queryClient}>
-          <AppRouterProvider />
-        </QueryClientProvider>
+        <CartContextProvider>
+          <QueryClientProvider client={queryClient}>
+            <AppRouterProvider />
+          </QueryClientProvider>
+        </CartContextProvider>
       </UserContextProvider>
     </ErrorBoundary>
   </StrictMode>
