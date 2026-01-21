6a61080b2bbf81bdf2227f8f8c2f56f5c59f6961
Cory House <housecor@gmail.com>
2026-01-21 12:22:05 -0600

    fix startup

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
+    command: "bun start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
