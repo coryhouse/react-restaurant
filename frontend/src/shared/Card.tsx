049505b2a4e9a6520cb18ffaa8411e79a55bd9a2
Cory House <housecor@gmail.com>
2026-02-06 15:56:54 -0600

    fix color

--- frontend/src/shared/Card.tsx
+++ frontend/src/shared/Card.tsx
@@ -2,8 +2,8 @@ type CardProps = React.ComponentPropsWithoutRef<"section">;
 
 export function Card({ className, ...props }: CardProps) {
   const combinedClassName = className
-    ? `${className} bg-gray-100 rounded shadow border-1 border-gray-200 m-4 p-4 mw-96`
-    : "bg-gray-100 rounded shadow border-1 border-gray-200 m-4 p-4 mw-96";
+    ? `${className} rounded shadow border-1 border-gray-200 m-4 p-4 mw-96`
+    : "rounded shadow border-1 border-gray-200 m-4 p-4 mw-96";
 
   return <section className={combinedClassName} {...props} />;
 }
