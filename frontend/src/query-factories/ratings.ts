6e43aa146b95cf6ab7543901cafbf129620f6607
Cory House <housecor@gmail.com>
2026-05-14 15:46:16 -0500

    fix 404

--- frontend/src/query-factories/ratings.ts
+++ frontend/src/query-factories/ratings.ts
@@ -1,6 +1,6 @@
 // This file contains reusable Tanstack Query QueryOptions. See here: https://tkdodo.eu/blog/the-query-options-api
 import { queryOptions } from "@tanstack/react-query";
-import ky from "ky";
+import ky, { HTTPError } from "ky";
 import { ratingSchema } from "../types/rating.types";
 
 const baseUrl = import.meta.env.VITE_API_URL + "/ratings";
@@ -9,22 +9,20 @@ const keys = {
   allRatings: ["ratings"],
 };
 
-export class RatingNotFoundError extends Error {}
-
 export const ratingQueries = {
   getRatingsByFoodId: (foodId?: number) =>
     queryOptions({
       queryKey: [...keys.allRatings, foodId],
       queryFn: async () => {
-        const json = await ky
-          .get(`${baseUrl}?foodId=${foodId}`)
-          .json()
-          .catch((error) => {
-            if (error.response.status === 404)
-              throw new RatingNotFoundError("Rating not found");
-            throw new Error("Failed to fetch rating");
-          });
-        return ratingSchema.array().parse(json);
+        try {
+          const json = await ky.get(`${baseUrl}?foodId=${foodId}`).json();
+          return ratingSchema.array().parse(json);
+        } catch (error) {
+          if (error instanceof HTTPError && error.response.status === 404) {
+            return [];
+          }
+          throw error;
+        }
       },
     }),
 };
