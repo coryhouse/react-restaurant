48b4cc8d5b64da032a6ed2d789a084cc5b2a2102
Cory House <housecor@gmail.com>
2026-05-14 16:38:15 -0500

    style tweaks

--- frontend/src/shared/Card.tsx
+++ frontend/src/shared/Card.tsx
@@ -2,8 +2,8 @@ type CardProps = React.ComponentPropsWithoutRef<"section">;
 
 export function Card({ className, ...props }: CardProps) {
   const combinedClassName = className
-    ? `${className} rounded shadow border-1 border-gray-200 m-4 p-4 mw-96`
-    : "rounded shadow border-1 border-gray-200 m-4 p-4 mw-96";
+    ? `${className} rounded shadow border border-gray-200 m-4 p-4 max-w-96`
+    : "rounded shadow border border-gray-200 m-4 p-4 max-w-96";
 
   return <section className={combinedClassName} {...props} />;
 }
